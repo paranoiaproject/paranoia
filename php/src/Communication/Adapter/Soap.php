89cc1c898ff01ff945c3423d22ed143dda52e475
GoktugOzturk <goktug.ozturk@gmail.com>
2013-07-16 09:38:35 -0430

    required function sendRequest implemented for Soap Adapter
    Change-Id: Id4f7ffdaef1f905b029d4818e1e07d1d5bc60bf1

--- php/src/Communication/Adapter/Soap.php
+++ php/src/Communication/Adapter/Soap.php
@@ -2,12 +2,15 @@
 namespace Communication\Adapter;
 
 use \Communication\Adapter\AdapterInterface;
-
 use \Communication\Exception\UndefinedHttpMethod;
 use \Communication\Exception\CommunicationFailed;
 
 class Soap implements AdapterInterface
 {
-
+    /* @see \Communication\CommunicationInterface::sendRequest() */
+    public function sendRequest($url, $data, $options = null)
+    {
+        throw new CommunicationFailed('Not Implemented Yet!');
+    }
 }
 
